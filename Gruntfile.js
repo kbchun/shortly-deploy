0f2462b1d4c312ca8d46b28c62a253e0c1a45975
Hack Reactor Students <accounts@hackreactor.com>
2016-10-12 15:49:15 -0700

    Implement Gruntfile and change client dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,15 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
+    concat: { // which files to concat together?
+      lib: { // what does this mean?
+        src: ['public/lib/jquery.js', 'public/lib/underscore.js', 'public/lib/backbone.js', 'public/lib/handlebars.js'],
+        dest: 'public/dist/libraries.js',
+      },
+      client: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/client.js'
+      }
     },
 
     mochaTest: {
@@ -20,22 +28,38 @@ module.exports = function(grunt) {
       }
     },
 
-    uglify: {
+    uglify: { // TODO test individually
+      my_target: {
+        files: {
+          'public/dist/libraries_min.js': ['public/dist/libraries.js'], // what about multiple files?
+          'public/dist/client_min.js': ['public/dist/client.js']
+        }
+      }
+    },
+
+    clean: {
+      contents: ['public/dist/*']
     },
 
     eslint: {
       target: [
         // Add list of files to lint here
+        // TODO come back when we're Mongo-ing
       ]
     },
 
     cssmin: {
+      target: {
+        files: {
+          'public/dist/style_min.css': ['public/style.css']
+        }
+      }
     },
 
     watch: {
       scripts: {
         files: [
-          'public/client/**/*.js',
+          'public/client/**/*.js', // why watch client?
           'public/lib/**/*.js',
         ],
         tasks: [
@@ -51,10 +75,17 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
+        command: 'git push live master',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
       }
     },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -73,15 +104,21 @@ module.exports = function(grunt) {
   ////////////////////////////////////////////////////
 
   grunt.registerTask('test', [
+    'eslint',
     'mochaTest'
   ]);
 
   grunt.registerTask('build', [
+    'clean',
+    'concat',
+    'uglify',
+    'cssmin'
   ]);
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
       // add your production server task here
+      grunt.task.run(['shell:prodServer']);
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
@@ -89,6 +126,9 @@ module.exports = function(grunt) {
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
+    //'test',
+    'build',
+    'upload'
   ]);
 
 
